7d1669e2572b4ba0d521bd05730e4c6c5074d01f
yoseiyoseiyosei <fairylandcompany@gmail.com>
2014-09-15 15:53:37 +0800

    スワイプ

--- SwipeCode/ViewController.m
+++ SwipeCode/ViewController.m
@@ -17,13 +17,59 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    UIImage *image = [UIImage imageNamed:@"むきむき.jpeg"];
+    UIImageView *iv = [[UIImageView alloc] initWithImage:image];
+    [self.view addSubview:iv];
+    
+    
+    //上方向スワイプの認識
+    UISwipeGestureRecognizer *swipeUpRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self.view action:@selector(handleSwipeUpGesture:)];
+    swipeUpRecognizer.delegate = self;
+    swipeUpRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
+    [self.view addGestureRecognizer:swipeUpRecognizer];
+   //下方向スワイプの認識
+    UISwipeGestureRecognizer *swipeDownRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self.view action:@selector(handleSwipeDownGesture:)];
+    swipeDownRecognizer.delegate = self;
+    swipeDownRecognizer.direction = UISwipeGestureRecognizerDirectionDown;
+    [self.view addGestureRecognizer:swipeDownRecognizer];
+
+    // 左へスワイプ
+    UISwipeGestureRecognizer* swipeLeftGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeLeftGesture:)];
+    swipeLeftGesture.delegate = self;
+    swipeLeftGesture.direction = UISwipeGestureRecognizerDirectionLeft;
+    [self.view addGestureRecognizer:swipeLeftGesture];
+    // 右へスワイプ
+    UISwipeGestureRecognizer* swipeRightGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeRightGesture:)];
+    swipeRightGesture.delegate = self;
+    swipeRightGesture.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:swipeRightGesture];
+    
 }
+
+
+- (IBAction)handleSwipeLeftGesture:(UISwipeGestureRecognizer *)sender {
+    NSLog(@"swipe left");
+}
+
+-(IBAction)handleSwipeRightGesture:(UISwipeGestureRecognizer *)sender{
+    NSLog(@"swipe right");
+}
+
+-(IBAction) handleSwipeUpGesture:(UISwipeGestureRecognizer *)sender {
+    NSLog(@"swipe up");
+}
+
+-(IBAction) handleSwipeDownGesture:(UISwipeGestureRecognizer *)sender {
+    NSLog(@"swipe down");
+}
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
 @end
